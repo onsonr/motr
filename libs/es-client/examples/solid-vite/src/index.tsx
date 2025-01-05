6f5acfd608095349203b8b8701a4bff81f2045ca
Darp Alakun <i@prad.nu>
2025-01-04 20:56:11 -0500

    refactor: remove example projects

--- libs/es-client/examples/solid-vite/src/index.tsx
+++
@@ -1,8 +0,0 @@
-/* @refresh reload */
-import { render } from "solid-js/web";
-import "./polyfill"; // ! register polyfill before any other imports
-
-import App from "./App";
-import "./index.css";
-
-render(() => <App />, document.getElementById("root")!);
--- libs/es-client/examples/solid-vite/tailwind.config.cjs
+++
@@ -1,4 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-};
